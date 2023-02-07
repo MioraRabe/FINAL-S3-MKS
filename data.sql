4739b9a4642e0e14426c692651f6d10d89744a98
MioraRabe <120572809+MioraRabe@users.noreply.github.com>
2023-02-08 00:13:44 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,9 +1,9 @@
 use takalo;
 
 insert into Users values(null, 'Admin', 'admin@gmail.com', 'admin', 0);
-insert into Users values(null, 'Toky', 'toky@gmail.com', 'toky', 1);
-insert into Users values(null, 'Angelo', 'angelo@gmail.com', 'angelo', 1);
-insert into Users values(null, 'Mathias', 'mathias@gmail.com', 'mathias', 1);
+insert into Users values(null, 'Miora', 'miora@gmail.com', 'miora', 1);
+insert into Users values(null, 'Kathy', 'kathy@gmail.com', 'kathy', 1);
+insert into Users values(null, 'Sandratra', 'sandratra@gmail.com', 'sandratra', 1);
 
 insert into Categorie values(null, 'Vetements');
 insert into Categorie values(null, 'Livre');
@@ -17,7 +17,7 @@ insert into Objet values(null, 'veste en Jean', '1 denim Jacket de chez Levis', 
 insert into Objet values(null, 'Roman d un non-mort', 'Roman fantastique jeunesse sorti en 2014', 2, 10000, 'Livre/2.png');
 insert into Objet values(null, 'tablette graphique Wacon Intuos', 'fonctionne par bluetooth. 7 pouces. 249g', 3, 400000, 'Informatique/2.png');
 insert into Objet values(null, 'jupe carreaux', 'Jupe plissée courte Taille Haute pour femme', 1, 30000 , 'Vetements/3.png');
-insert into Objet values(null, 'Vingt mille lieue sous la mer', 'Roman d'aventure, anticipation fantastique', 2, 20000, 'Livre/3.png');
+insert into Objet values(null, 'Vingt mille lieue sous la mer', 'Roman d aventure, anticipation fantastique', 2, 20000, 'Livre/3.png');
 insert into Objet values(null, 'Souris', 'KLIM Souris Gamer Haute Précision', 3, 40000, 'Informatique/3.png');
 
 insert into Proprio values(1, 1, 2022-10-12);

d8cf202a46b4ece31f6c500c6b0bbb47d4862035
MioraRabe <120572809+MioraRabe@users.noreply.github.com>
2023-02-07 21:38:17 +0300

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,12 @@
+use takalo;
+
+insert into Users values(null, 'Admin', 'admin@gmail.com', 'admin', 0);
+insert into Users values(null, 'Toky', 'toky@gmail.com', 'toky', 1);
+insert into Users values(null, 'Angelo', 'angelo@gmail.com', 'angelo', 1);
+insert into Users values(null, 'Mathias', 'mathias@gmail.com', 'mathias', 1);
+
+insert into Categorie values(null, 'Vetements');
+insert into Categorie values(null, 'Livre');
+insert into Categorie values(null, 'Informatique');
+
+insert into Objet values(null, 'Tshirt TNF', 'Tshirt blanc avec logo the north face tendance jeune', 1, 15000, 'Vetements/1.png');
